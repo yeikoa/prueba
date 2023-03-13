466a84b0fb235260046d61a4c3cef3a71282636d
Yeiko Artavia <yeiko2003@gmail.com>
2023-03-13 00:07:15 -0600

    actualizando

--- js/polimParametrico.js
+++ js/polimParametrico.js
@@ -1,1 +1,17 @@
 //Parametros de cualquier tipo(Genericos)
+function Stack(){
+    this.items = [];
+
+    this.push = function(item){
+        this.items.push(item);
+    }
+
+
+}
+const stack = new Stack();
+stack.push("yeikoooo");
+const stack2 = new Stack();
+stack2.push(200);
+
+console.log(stack);
+console.log(stack2);
